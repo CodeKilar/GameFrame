7f6e4a8197b981804219cda80563216b047421e2
CodeKilar <834663045@qq.com>
2026-03-29 23:19:39 +0800

    feat：更新单线程定时器

---
+++ frame/timer/main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	mock_simpleTimer()
+}
--- frame/timer/simple_timer.go
+++ frame/timer/simple_timer.go
@@ -1,1 +1,61 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Task struct {
+	ExecuteAt time.Time
+	callback  func()
+}
+
+type SimpleTimer struct {
+	tasks []*Task
+}
+
+func NewSimpleTimer() *SimpleTimer {
+	return &SimpleTimer{
+		tasks: []*Task{},
+	}
+}
+
+func (s *SimpleTimer) AddTask(delay time.Duration, cb func()) {
+	s.tasks = append(s.tasks, &Task{
+		ExecuteAt: time.Now().Add(delay),
+		callback:  cb,
+	})
+}
+
+func (s *SimpleTimer) loop() {
+	fmt.Println("begin loop")
+	for {
+		var earliest *Task
+		var index int
+		for i, v := range s.tasks {
+			if earliest == nil || v.ExecuteAt.Before(earliest.ExecuteAt) {
+				earliest = v
+				index = i
+			}
+		}
+
+		if earliest == nil {
+			break
+		}
+
+		wait := time.Until(earliest.ExecuteAt)
+		time.Sleep(wait)
+
+		earliest.callback()
+
+		s.tasks = append(s.tasks[:index], s.tasks[index+1:]...)
+	}
+}
+
+func mock_simpleTimer() {
+
+	s := NewSimpleTimer()
+	// 添加定时器
+	s.AddTask(2*time.Second, func() { fmt.Println("2s later") })
+	s.loop()
+}
